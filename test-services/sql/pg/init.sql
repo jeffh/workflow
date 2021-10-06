a09595654d8371f339fde7c999da9111b87f4071
Jeff Hui <jeff@jeffhui.net>
2021-10-06 00:42:26 -0700

    Split test + dev databases

---
+++ test-services/sql/pg/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE workflow_dev;
+CREATE DATABASE workflow_test;
+GRANT ALL PRIVILEGES ON DATABASE workflow_dev TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE workflow_test TO postgres;
